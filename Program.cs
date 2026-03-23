33b79b9f05bf599079107cdb375a7ea6fe30655a
Anna <anyuta.sokolova2008@gmail.com>
2026-03-23 15:40:32 +0300

    Первый коммит

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Kvitanция
+{
+    public int Number;
+    public string Date;
+    public float Sum;
+
+    public Kvitanция(int number, string date, float sum)
+    {
+        Number = number;
+        Date = date;
+        Sum = sum;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kvitanция kv = new Kvitanция(1, "23.03.2026", 1500.50f);
+
+        Console.WriteLine("Номер: " + kv.Number);
+        Console.WriteLine("Дата: " + kv.Date);
+        Console.WriteLine("Сумма: " + kv.Sum);
+    }
+}
